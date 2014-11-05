e19b15694d86967448567bfab8ef6493c7a71fde
jordyijsselstijn <jordyijsselstijn@gmail.com>
2014-11-05 22:42:58 +0100

    Small changes since last commit

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
 
 
-
-    if(window.innerWidth<768){
-
-        $("#temp-module-img").addClass("hidden");
-        $("#hum-module-img").addClass("hidden");
-        $("#graph-module-img").addClass("hidden");
-
-    }
-
-
+    //
+    //if(window.innerWidth<768){
+    //
+    //    $("#temp-module-img").addClass("hidden");
+    //    $("#hum-module-img").addClass("hidden");
+    //    $("#graph-module-img").addClass("hidden");
+    //
+    //}
+    //
+    //
 
 
 
