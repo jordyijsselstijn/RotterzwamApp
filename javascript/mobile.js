3b6890a5cd1750a18569f0db4be403402d94fa0a
jordyijsselstijn <jordyijsselstijn@gmail.com>
2014-11-05 22:28:48 +0100

    Added mobile compatibility to the project.

---
+++ javascript/mobile.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+
+
+    if(window.innerWidth<768){
+
+        $("#temp-module-img").addClass("hidden");
+        $("#hum-module-img").addClass("hidden");
+        $("#graph-module-img").addClass("hidden");
+
+    }
+
+
+
+
+
+});
